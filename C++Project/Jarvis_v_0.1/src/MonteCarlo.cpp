1468f1169a784a6db8b1e3f1fb0a5bc799e7d77b
cchatell <corentin.chatellier@insa-rennes.fr>
2016-04-27 12:10:26 +0200

    launch

--- C++Project/Jarvis_v_0.1/src/MonteCarlo.cpp
+++ C++Project/Jarvis_v_0.1/src/MonteCarlo.cpp
@@ -1,6 +1,8 @@
 #include "MonteCarlo.h"
+#include <iostream>
 
-MonteCarlo::MonteCarlo()
+using namespace std;
+MonteCarlo::MonteCarlo(Game* g):m_game(g)
 {
     //ctor
 }
@@ -11,6 +13,14 @@ MonteCarlo::~MonteCarlo()
 }
 
 Card* MonteCarlo::launch(){
+    int i=0;
+    for (i=0;i<8;i++){
+        cout << m_game->toString()<<endl;
+       Game g1=*m_game;
+        g1.getHand(0)->discard(0);
+        cout << m_game->toString()<<endl;
+        Card* cardToTest = g1.getHand(0)->getCard(i);
 
-    return 0;
+        return 0;
+    }
 }
