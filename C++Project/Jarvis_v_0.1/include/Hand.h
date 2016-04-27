39725b67c1eff243cfe2bb9ca20480118dd7cba0
cchatell <corentin.chatellier@insa-rennes.fr>
2016-04-27 17:17:50 +0200

    copie de Hand marche bien mais pas copie de game

--- C++Project/Jarvis_v_0.1/include/Hand.h
+++ C++Project/Jarvis_v_0.1/include/Hand.h
@@ -21,13 +21,16 @@ class Hand
         Card* discard(int index);
 
         // gets the card #index from m_hands (not deleted)
-        Card* getCard(int index);
+        Card* getCard (int index)const ;
+        void setCards(std::vector<Card*> c){m_cards=c;};
+        std::vector<Card*> getCards() const {return m_cards;};
 
         std::string toString();
 
-        int getNumber(){return m_number;};
+        int getNumber() const {return m_number;};
+        void setNumber(int n){m_number=n;};
 
-        virtual Card* play()=0;
+        virtual Card* play(){ return 0;};
     protected:
     private:
         std::vector<Card*> m_cards;
@@ -38,5 +41,8 @@ class Hand
 class IA_random : public Hand
 {
     public:
-        virtual Card* play();
+        IA_random(int n,Card *c1, Card *c2, Card *c3, Card *c4, Card *c5, Card *c6, Card *c7, Card *c8);
+        IA_random();
+        IA_random(const IA_random &i);
+        Card* play();
 };
